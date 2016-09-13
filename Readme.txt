1065bf347af5e910faa06785b26e4712722e14c4
Lonelyowo <378399872@qq.com>
2016-09-13 18:17:57 +0800

    chenjp
    chenjp web os

---
+++ Readme.txt
@@ -0,0 +1,58 @@
+chenjp/
+
+	-app/
+		-admin/
+			-...
+		-index/
+			-...
+	
+	-cache/
+		-admin/
+		-index/
+	
+	-data/ 
+		-admin/
+			-session/ 后台session目录
+		-index/
+			-session/ 前台session目录
+	
+	-kdr/ 编辑器目录
+	
+	-logs/
+		-admin/	后台log目录
+		-index/	前台log目录
+	
+	-public_config/
+		-db_config.php	网站公用数据库配置文件
+	
+	-static/	静态资源文件目录
+		-admin/
+			-css/
+			-js/
+		-index/
+			-css/
+			-js/
+	
+	-system/
+	
+	-uploads/
+		-admin/
+		-index/
+	
+	-views/	
+		-admin/	后台html界面模版目录
+		-index/	前台html界面模版目录
+	
+	-admin.php	后台入口
+	
+	-index.php	前台入口
+
+
+
+
+
+
+
+
+
+	
---
+++ public_config/db_config.php
@@ -0,0 +1,26 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*----------------------------网站公用数据库配置文件------------------------*/
+
+$db['default'] = array(
+	'dsn'	=> '',
+	'hostname' => 'localhost',
+	'username' => 'root',
+	'password' => '',
+	'database' => 'club',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
